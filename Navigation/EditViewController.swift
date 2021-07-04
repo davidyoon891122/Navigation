25defa80c9a43bb1fba77fd18e716688a6df1931
davidyoon <qqpo12@naver.com>
2021-07-04 19:46:03 +0900

    add comments

--- Navigation/EditViewController.swift
+++ Navigation/EditViewController.swift
@@ -6,21 +6,37 @@
 //
 
 import UIKit
+// 인터페이스 처럼 사용 되는 protocol
+protocol EditDelegate {
+    func didMessageEditDone(_ controller: EditViewController, message: String)
+}
 
 class EditViewController: UIViewController {
 
     @IBOutlet var lblWay: UILabel!
     // ViewController에서 EditViewController의 뷰에는 직접 접근이 안되기 떄문에 변수 선언해서 해당 변수에 접근하여 데이터 삽입
     var textWayValue: String = ""
+    var textMessage: String = ""
+    // delegate 변수
+    var delegate: EditDelegate?
+    @IBOutlet var txtMessage: UITextField!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         lblWay.text = textWayValue
         // Do any additional setup after loading the view.
+        // Main에서 직접 뷰에 접근 못하기 때문에 텍스트 변수에 데이터 전달
+        txtMessage.text = textMessage
     }
     
     // 버튼 클릭 시 navigationController를 사용 하여 현재 화면을 pop
     @IBAction func btnDone(_ sender: UIButton) {
+        // btnDone 버튼 클릭시 deletgate 있으면 didMessageEdiotDone 함수 호출(자신과 자신의 UITextField에 적혀진 text 전달
+        if delegate != nil {
+            delegate?.didMessageEditDone(self, message: txtMessage.text!)
+        }
+        
         _ = navigationController?.popViewController(animated: true)
     }
     /*
--- Navigation/ViewController.swift
+++ Navigation/ViewController.swift
@@ -6,9 +6,9 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
-
+// EditViewController에서 만든 EditDelegate protocol 상속
+class ViewController: UIViewController, EditDelegate {
+    @IBOutlet var txtMessage: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -21,6 +21,14 @@ class ViewController: UIViewController {
         } else if (segue.identifier == "editBarButton") {
             editViewController.textWayValue = "segue: use Bar button"
         }
+        // 메인에서 수정으로 화면 넘길때 editViewContoller.textMessage에 자신의 UITextField 텍스트값 전달
+        editViewController.textMessage = txtMessage.text!
+        //editViewController.delegate에 자신 선언
+        editViewController.delegate = self
+    }
+    // protocol에서 정의된 함수 정의 
+    func didMessageEditDone(_ controller: EditViewController, message: String) {
+        txtMessage.text = message
     }
 }
 
